b91863eaf4070e36917bded7ef0043edd60ff3c5
Eric Daams <eric@ericnicolaas.com>
2013-06-20 16:05:55 +0930

    Adding background texture. Changing greys to warmer browny shade

--- campaign-blurb.php
+++ campaign-blurb.php
@@ -7,46 +7,50 @@
 <!-- Active campaign -->
 <section class="active-campaign cf">
 
-	<div class="campaign-excerpt"><?php the_excerpt() ?></div>	
+	<div class="shadow-wrapper">
 
-	<?php if ( has_post_thumbnail( $campaign->ID ) ) : ?>
-		<?php echo get_the_post_thumbnail( $campaign->ID ) ?>
-	<?php endif ?>
+		<div class="campaign-excerpt"><?php the_excerpt() ?></div>	
 
-	<div class="campaign-summary cf">		
+		<?php if ( has_post_thumbnail( $campaign->ID ) ) : ?>
+			<?php echo get_the_post_thumbnail( $campaign->ID ) ?>
+		<?php endif ?>
 
-		<p class="campaign-support"><a class="button button-large" data-reveal-id="campaign-form" href="#"><?php _e( 'Support', 'projection' ) ?></a></p>
+		<div class="campaign-summary cf">		
 
-		<div class="barometer">
-			<span><?php printf( _x( "%s Funded", 'x percent funded', 'projection' ), '<span>'.$campaign->percent_completed(true).'</span>' ) ?></span>
-		</div>		
+			<p class="campaign-support"><a class="button button-large" data-reveal-id="campaign-form" href="#"><?php _e( 'Support', 'projection' ) ?></a></p>
 
-		<ul>
-			<li class="campaign-raised">
-				<span><?php echo $campaign->current_amount() ?></span>
-				<?php _e( 'Pledged', 'projection' ) ?>		
-			</li>
-			<li class="campaign-goal">
-				<span><?php echo $campaign->goal() ?></span>
-				<?php _e( 'Goal', 'projection' ) ?>				
-			</li>
-			<li class="campaign-backers">
-				<span><?php echo $campaign->backers_count() ?></span>
-				<?php _e( 'Backers', 'projection' ) ?>
-			</li>
-			
-<!-- 			<li class="campaign-end">
-				<span><?php _e( 'End date', 'projection' ) ?></span>
-				<?php echo mysql2date( 'j F, Y', $campaign->__get( 'campaign_end_date' ) ) ?>
-			</li> -->			
-		</ul>
+			<div class="barometer">
+				<span><?php printf( _x( "%s Funded", 'x percent funded', 'projection' ), '<span>'.$campaign->percent_completed(true).'</span>' ) ?></span>
+			</div>		
 
-		<div class="campaign-countdown">
-			<span class="countdown" data-enddate='<?php echo sofa_crowdfunding_get_enddate($campaign, true) ?>'></span>
-			<span><?php _e( 'Time left to donate', 'projection' ) ?></span>
+			<ul>
+				<li class="campaign-raised">
+					<span><?php echo $campaign->current_amount() ?></span>
+					<?php _e( 'Pledged', 'projection' ) ?>		
+				</li>
+				<li class="campaign-goal">
+					<span><?php echo $campaign->goal() ?></span>
+					<?php _e( 'Goal', 'projection' ) ?>				
+				</li>
+				<li class="campaign-backers">
+					<span><?php echo $campaign->backers_count() ?></span>
+					<?php _e( 'Backers', 'projection' ) ?>
+				</li>
+				
+	<!-- 			<li class="campaign-end">
+					<span><?php _e( 'End date', 'projection' ) ?></span>
+					<?php echo mysql2date( 'j F, Y', $campaign->__get( 'campaign_end_date' ) ) ?>
+				</li> -->			
+			</ul>
+
+			<div class="campaign-countdown">
+				<span class="countdown" data-enddate='<?php echo sofa_crowdfunding_get_enddate($campaign, true) ?>'></span>
+				<span><?php _e( 'Time left to donate', 'projection' ) ?></span>
+			</div>
+
+			<?php //echo projection_get_enddate_json($campaign, true) ?>		
+
 		</div>
-
-		<?php //echo projection_get_enddate_json($campaign, true) ?>		
 
 	</div>	
 
