79be592c415c1685bb0cf2cbcb484663d2bc4b97
Eric Daams <eric@ericnicolaas.com>
2013-06-21 13:52:49 +0930

    Some fixes

--- footer.php
+++ footer.php
@@ -2,19 +2,13 @@
 		<!-- End main content section -->
 		
 		<!-- Site footer -->
-		<footer id="site_footer" class="wrapper">
+		<footer id="site-footer" class="wrapper">
 
 			<div id="footer_widgets" class="inner_wrap widget_quatro cf">
 
 				<?php dynamic_sidebar( 'footer' ) ?>			
 
 			</div>
-
-			<!-- You've hit rockbottom -->
-			<div id="rockbottom">
-				<p class="footer_notice aligncenter"><?php echo get_theme_mod( 'footer_notice' ) ?></p>			
-			</div>
-			<!-- You've passed rockbottom -->
 
 		</footer>		
 
